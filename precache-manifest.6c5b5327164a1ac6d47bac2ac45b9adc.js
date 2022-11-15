46cc19b32d3fd67d1e934e38ef4547525bb35dc5
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2022-11-16 00:48:31 +0530

    Updates

---
+++ precache-manifest.6c5b5327164a1ac6d47bac2ac45b9adc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4c685da73c310b6ed1507d0676e211",
+    "url": "/advice-app/index.html"
+  },
+  {
+    "revision": "5ac2543ddd93ff117915",
+    "url": "/advice-app/static/css/main.5de97074.chunk.css"
+  },
+  {
+    "revision": "be8764e90a80b8a08290",
+    "url": "/advice-app/static/js/2.bf44d81a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/advice-app/static/js/2.bf44d81a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac2543ddd93ff117915",
+    "url": "/advice-app/static/js/main.52eb2951.chunk.js"
+  },
+  {
+    "revision": "071582da113a2689bc6a",
+    "url": "/advice-app/static/js/runtime-main.6e9875f8.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/advice-app/static/media/city.91c109fc.jpg"
+  }
+]);
